b42d5653e4b74fa8a02820212712bd330c0e27d4
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-05-18 18:15:20 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -3,8 +3,7 @@
 struct Stats compute_statistics(const float* numberset, int setlength)
 {
     	float result;
-	float minimum[1] = {0};
-	float maximum[1] = {0};
+	float minimum, maximum;
 	
         struct Stats s;
 	
@@ -20,9 +19,9 @@ struct Stats compute_statistics(const float* numberset, int setlength)
 	//else
 	//{
 		s.average = calculate_average(numberset, setlength, result);
-		calculate_min_max(numberset, setlength, minimum, maximum);
-		s.min = minimum[0] ;
-    		s.max = maximum[0] ;
+		calculate_min_max(numberset, setlength, &minimum, &maximum);
+		s.min = *minimum ;
+    		s.max = *maximum ;
 		
 		return s;		
 	//}' 
@@ -44,19 +43,19 @@ return (result/setlength);
 void calculate_min_max(const float* numberset, int setlength, float *minimum, float *maximum)
 {
     int i;
-	minimum[0] = numberset[0];
-	maximum[0] = numberset[0];
+	*minimum = numberset[0];
+	*maximum = numberset[0];
 	
 	
 	for(i=1; i<setlength; i++)
 	{
-		if(minimum[0]  > numberset[i] )
+		if*(minimum  > numberset[i] )
 		{
-			minimum[0]  = numberset[i];
+			*minimum  = numberset[i];
 		}
-		else if(maximum[0] < numberset[i] )
+		else if(*maximum < numberset[i] )
 		{
-			maximum[0] = numberset[i];
+			*maximum = numberset[i];
 		}
 	}
    
